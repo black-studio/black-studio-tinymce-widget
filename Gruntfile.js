b6df01f5c7234ab2c9ba056f29200a49921f45bc
Marco Chiesi <marco@blackstudio.it>
2014-08-23 00:55:33 +0200

    Updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,28 +57,34 @@ module.exports = function( grunt ) {
 					'<%= dirs.js %>/*.js',
 				],
 				tasks: ['uglify']
+			},
+			makepot: {
+				files: '**/*.php',
+				tasks: ['makepot']
 			}
 		},
 
 		makepot: {
-			options: {
-				type: 'wp-plugin',
-				domainPath: 'languages',
-				potHeaders: {
-					'report-msgid-bugs-to': 'https://github.com/black-studio/black-studio-tinymce-widget/issues',
-					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
-				}
-			},
-			admin: {
+			target: {
 				options: {
+					domainPath: '/languages',
 					potFilename: 'black-studio-tinymce-widget.pot',
-					include: [
-						'.*'
-					],
-					processPot: function ( pot ) {
-						pot.headers['project-id-version'] += ' Admin';
+					processPot: function( pot, options ) {
+						pot.headers['report-msgid-bugs-to'] = 'https://github.com/black-studio/black-studio-tinymce-widget/issues\n';
+						pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
+						pot.headers['last-translator'] = 'Black Studio <info@blackstudio.it>\n';
+						pot.headers['language-team'] = 'Black Studio <info@blackstudio.it>\n';
+						pot.headers['x-poedit-basepath'] = '.\n';
+						pot.headers['x-poedit-language'] = 'English\n';
+						pot.headers['x-poedit-country'] = 'UNITED STATES\n';
+						pot.headers['x-poedit-sourcecharset'] = 'utf-8\n';
+						pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c,_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
+						pot.headers['x-poedit-bookmarks'] = '\n';
+						pot.headers['x-poedit-searchpath-0'] = '.\n';
+						pot.headers['x-textdomain-support'] = 'yes\n';
 						return pot;
-					}
+					},
+					type: 'wp-plugin'
 				}
 			}
 		},
@@ -112,6 +118,13 @@ module.exports = function( grunt ) {
 			}
 		},
 
+		po2mo: {
+			files: {
+				src: 'languages/*.po',
+				expand: true,
+			},
+		},
+
 		wp_readme_to_markdown: {
 			convert: {
 				files: {
@@ -138,6 +151,7 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks('grunt-po2mo');
 
 	// Register tasks
 	grunt.registerTask( 'default', [
