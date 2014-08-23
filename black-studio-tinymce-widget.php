black-studio/black-studio-tinymce-widget

commit 82c16aab5c61510f46eed80fc79110f071ad9e35

classes added to the object for access + typo fix

black-studio-tinymce-widget.php
@@ -54,8 +54,8 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Plugin' ) ) {
 			add_filter( '_upload_iframe_src', array( $this, '_upload_iframe_src' ) );
 			add_filter( 'wp_default_editor', array( $this, 'editor_accessibility_mode' ) );
 			// Handle compatibility code
-			new Black_Studio_TinyMCE_Compatibility_Wordpress( $this );
-			new Black_Studio_TinyMCE_Compatibility_Plugins();
+			self::$_instance->compat_plugins = new Black_Studio_TinyMCE_Compatibility_Plugins();
+			self::$_instance->compat_wp      = new Black_Studio_TinyMCE_Compatibility_Wordpress( self::$_instance );
 		}
 
 		/* Get plugin version */

classes/class-compatibility-wordpress.php
@@ -47,7 +47,7 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Wordpress' ) ) {
 		}
 
 		/* Enqueue styles for WordPress prior to 3.8 */
-		function wp_pre_38_style( ) {
+		function wp_pre_38_style() {
 			return 'black-studio-tinymce-widget-legacy';
 		}
 
@@ -106,7 +106,7 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Wordpress' ) ) {
 
 		/* Compatibility for WordPress prior to 3.2 */
 		function wp_pre_32() {
-			remove_action( 'admin_print_footer_scripts', array( $thi, 'wp_pre_33_admin_print_footer_scripts' ) );
+			remove_action( 'admin_print_footer_scripts', array( $this, 'wp_pre_33_admin_print_footer_scripts' ) );
 			add_action( 'admin_print_footer_scripts', array( $this, 'wp_pre_32_admin_print_footer_scripts' ) );
 		}
 

commit cd06b880461cf1e2e4e08375d05f8222e3523030

fix singleton error introduced in 82c16aab5c61510f46eed80fc79110f071ad9e35

black-studio-tinymce-widget.php
@@ -41,7 +41,7 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Plugin' ) ) {
 		}
 
 		/* Class constructor */
-		public function __construct() {
+		protected function __construct() {
 			// Include required files
 			$this->includes();
 			// Load localization
@@ -53,9 +53,11 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Plugin' ) ) {
 			add_filter( 'widget_text', array( $this, 'apply_smilies_to_widget_text' ) );
 			add_filter( '_upload_iframe_src', array( $this, '_upload_iframe_src' ) );
 			add_filter( 'wp_default_editor', array( $this, 'editor_accessibility_mode' ) );
-			// Handle compatibility code
-			self::$_instance->compat_plugins = new Black_Studio_TinyMCE_Compatibility_Plugins();
-			self::$_instance->compat_wp      = new Black_Studio_TinyMCE_Compatibility_Wordpress( self::$_instance );
+		}
+
+		protected function __clone() {
+			// Cloning instances of the class is forbidden
+			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; uh?' ), '2.0' );
 		}
 
 		/* Get plugin version */

classes/class-compatibility-plugins.php
@@ -72,3 +72,5 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Plugins' ) ) {
 	} // class declaration
 
 } // class_exists check
+
+return new Black_Studio_TinyMCE_Compatibility_Plugins();

classes/class-compatibility-wordpress.php
@@ -123,3 +123,5 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Wordpress' ) ) {
 	} // class declaration
 
 } // class_exists check
+
+return new Black_Studio_TinyMCE_Compatibility_Wordpress( bstw() );