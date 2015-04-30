3c5b323afd295011d4e202e1360caf2e7fa129d8
andreamk <andleo75@gmail.com>
2015-04-30 12:02:22 +0200

    Update black-studio-tinymce-widget.php

--- black-studio-tinymce-widget.php
+++ black-studio-tinymce-widget.php
@@ -236,7 +236,7 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Plugin' ) ) {
 		 * @since 2.0.0
 		 */
 		public function check_widget( $widget ) {
-			return gettype( $widget ) == 'object' && get_class( $widget ) == 'WP_Widget_Black_Studio_TinyMCE';
+			return gettype( $widget ) == 'object' && (get_class( $widget ) == 'WP_Widget_Black_Studio_TinyMCE' || is_subclass_of($widget , 'WP_Widget_Black_Studio_TinyMCE'));
 		}
 
 	} // END class Black_Studio_TinyMCE_Plugin
