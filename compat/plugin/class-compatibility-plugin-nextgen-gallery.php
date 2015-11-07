fcbc5b59339bdf4f179d2274ee0fd4be8dcde39f
Marco Chiesi <marco@blackstudio.it>
2015-11-07 01:15:14 +0100

    Added support for NextGEN Gallery in Customizer

--- compat/plugin/class-compatibility-plugin-nextgen-gallery.php
+++ compat/plugin/class-compatibility-plugin-nextgen-gallery.php
@@ -50,6 +50,7 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Plugin_Nextgen_Gallery'
 			if ( is_admin() ) {
 				add_action( 'admin_init', array( $this, 'nextgen_gallery_admin_init' ) );
 			}
+			add_action( 'init', array( $this, 'nextgen_gallery_customizer_init' ), 20 );
 			add_filter( 'widget_text', array( $this, 'nextgen_gallery_widget_text' ) );
 		}
 
@@ -79,14 +80,14 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Plugin_Nextgen_Gallery'
 					if ( class_exists( 'M_Attach_To_Post' ) ) {
 						$ngg_module_attach_to_post = new M_Attach_To_Post();
 						add_action( 'admin_enqueue_scripts', array( $ngg_module_attach_to_post, '_enqueue_tinymce_resources' ) );
-						add_action( 'admin_enqueue_scripts', array( $this, 'nextgen_gallery_enqueue_style' ) );
+						add_action( 'admin_enqueue_scripts', array( $this, 'nextgen_gallery_admin_enqueue_style' ) );
 					}
 				}
 			}
 		}
 
 		/**
-		 * Enqueue style for NextGEN Gallery plugin 
+		 * Enqueue admin style for NextGEN Gallery plugin 
 		 *
 		 * @uses wp_enqueue_style()
 		 * @uses C_Router class (part of NextGEN Gallery)
@@ -94,7 +95,7 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Plugin_Nextgen_Gallery'
 		 * @return void
 		 * @since 2.3.0
 		 */
-		public function nextgen_gallery_enqueue_style() {
+		public function nextgen_gallery_admin_enqueue_style() {
 			if ( class_exists( 'C_Router' ) ) {
 				$router = C_Router::get_instance();
 				wp_enqueue_style( 'ngg_attach_to_post_dialog', $router->get_static_url( 'photocrati-attach_to_post#attach_to_post_dialog.css' ) );
@@ -102,8 +103,29 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Plugin_Nextgen_Gallery'
 		}
 
 		/**
+		 * Enqueue NextGEN Gallery frame communication script in Customizer
+		 *
+		 * @uses is_plugin_active()
+		 * @uses add_action()
+		 * @uses M_Frame_Communication class (part of NextGEN Gallery)
+		 *
+		 * @return void
+		 * @since 2.3.0
+		 */
+		public function nextgen_gallery_customizer_init() {
+			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+			if ( is_plugin_active( 'nextgen-gallery/nggallery.php' ) ) {
+				if ( class_exists( 'M_Frame_Communication' ) ) {
+					$ngg_module_frame_communication = new M_Frame_Communication();
+					add_action( 'customize_controls_enqueue_scripts', array( $ngg_module_frame_communication, 'enqueue_admin_scripts' ) );
+				}
+			}
+		}
+
+		/**
 		 * Widget text filter code for NextGEN Gallery plugin
 		 *
+		 * @uses is_plugin_active()
 		 * @uses M_Attach_To_Post class (part of NextGEN Gallery)
 		 *
 		 * @param string $content
@@ -111,9 +133,12 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Plugin_Nextgen_Gallery'
 		 * @since 2.3.0
 		 */
 		public function nextgen_gallery_widget_text( $content ) {
-			if ( class_exists( 'M_Attach_To_Post' ) ) {
-				$ngg_module_attach_to_post = new M_Attach_To_Post();
-				$content = $ngg_module_attach_to_post->substitute_placeholder_imgs( $content );
+			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+			if ( is_plugin_active( 'nextgen-gallery/nggallery.php' ) ) {
+				if ( class_exists( 'M_Attach_To_Post' ) ) {
+					$ngg_module_attach_to_post = new M_Attach_To_Post();
+					$content = $ngg_module_attach_to_post->substitute_placeholder_imgs( $content );
+				}
 			}
 			return $content;
 		}
