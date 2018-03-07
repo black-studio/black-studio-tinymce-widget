881a33cab3953df8718fb4e2ac5c98652e0344b0
Marco Chiesi <marco@blackstudio.it>
2018-03-07 23:20:49 +0100

    Updated makepot configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,7 @@ module.exports = function( grunt ) {
 				}]
 			}
 		},
-		
+
 		// PHP Code Sniffer
 		phpcs: {
 			application: {
@@ -111,7 +111,7 @@ module.exports = function( grunt ) {
 		clean: {
 			main: ['build/<%= pkg.name %>']
 		},
-		
+
 		// Copy the plugin to a versioned release directory
 		copy: {
 			main: {
@@ -153,7 +153,7 @@ module.exports = function( grunt ) {
 				}]
 			}
 		},
-		
+
 		// Create zip package
 		compress: {
 			main: {
@@ -184,7 +184,7 @@ module.exports = function( grunt ) {
 						pot.headers['x-poedit-language'] = 'English\n';
 						pot.headers['x-poedit-country'] = 'United States\n';
 						pot.headers['x-poedit-sourcecharset'] = 'utf-8\n';
-						pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c,_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
+						pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c,_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;esc_html__;esc_html_e;esc_attr__;esc_attr_e;\n';
 						pot.headers['x-poedit-bookmarks'] = '\n';
 						pot.headers['x-poedit-searchpath-0'] = '.\n';
 						pot.headers['x-textdomain-support'] = 'yes\n';
@@ -326,7 +326,7 @@ module.exports = function( grunt ) {
 
 	// Load NPM tasks to be used here
 	require( 'load-grunt-tasks' )( grunt );
-	
+
 	// Register tasks
 	grunt.registerTask( 'default', [
 		'jshint',
