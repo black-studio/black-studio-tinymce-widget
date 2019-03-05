4588f81d9fc701df256638dbbbe6e8ec2140e48e
Marco Chiesi <marco@blackstudio.it>
2019-03-06 00:34:37 +0100

    Compatibility fix for Elementor Page Builder

--- includes/class-compatibility-plugins.php
+++ includes/class-compatibility-plugins.php
@@ -521,7 +521,11 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Compatibility_Plugins' ) ) {
 		 * @since 2.5.0
 		 */
 		public function elementor() {
-			if ( is_admin() && isset( $_GET['action'] ) && 'elementor' == $_GET['action'] ) {
+			if ( 
+				is_admin() &&
+				isset( $_REQUEST['action'] ) &&
+				in_array( $_REQUEST['action'], array( 'elementor', 'elementor_ajax' ) )
+			) {
 				add_filter( 'black_studio_tinymce_enable', '__return_false', 100 );
 				add_action( 'widgets_init', array( $this, 'elementor_unregister_widget' ), 20 );
 			}
